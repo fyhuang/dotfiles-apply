d7fc56789c33220e30bb59a5b886bd2734f6923a
Frank Huang <me@fyhuang.com>
2025-09-01 14:29:22 -0700

    Generate intermediate Operations for easier visibility

--- apply.py
+++ apply.py
@@ -7,9 +7,12 @@ if sys.version_info[0] < 3:
 
 import os
 import os.path
-import glob
+import argparse
+import pprint
 from pathlib import Path
 from collections import namedtuple
+
+from typing import Tuple, List
 
 
 ################################
@@ -21,10 +24,16 @@ DotfileEntry = namedtuple(
 )
 
 
+Operation = namedtuple(
+    "Operation", ["action", "source_path", "dest_path"]
+)
+
+
 class Paths:
     def __init__(self, top=None, dest=None):
         if top is None:
-            top = Path(os.path.realpath(__file__)).parent
+            #top = Path(os.path.realpath(__file__)).parent
+            top = Path.cwd()
         self.top = Path(top)
 
         if dest is None:
@@ -34,8 +43,10 @@ class Paths:
     def homelinks(self):
         return self.top / "homelinks"
 
-    def paths_file(self):
-        return self.top / "paths"
+    def path_overrides(self) -> dict:
+        base = read_path_overrides(self.top / "path_overrides")
+        custom = read_path_overrides(self.top / "customs" / "path_overrides")
+        return base | custom
 
 
 ################################
@@ -43,41 +54,47 @@ class Paths:
 ################################
 
 
+def read_path_overrides(filepath: Path) -> dict:
+    if not filepath.exists():
+        return {}
+    with open(filepath) as f:
+        return dict(line.strip().split(":", maxsplit=1) for line in f if ":" in line)
+
+
 def get_dotfile_entries(paths):
-    def search_recur(top):
-        filenames = []
-        entries = os.listdir(top)
-        for fn in (os.path.join(top, e) for e in entries):
-            if os.path.basename(fn) == "_link_individual":
+    overrides = paths.path_overrides()
+
+    source_paths = []
+    relative_paths = []
+    target_paths = []
+
+    def search_recur(top: Path):
+        for child in top.iterdir():
+            if child.name == "_link_individual":
                 continue
 
-            if os.path.isdir(fn):
+            if child.is_dir():
                 # we'll link the whole dir, unless _link_individual file is present
-                if os.path.exists(os.path.join(fn, "_link_individual")):
-                    filenames += search_recur(fn)
+                if (child / "_link_individual").exists():
+                    search_recur(child)
                     continue
-            filenames.append(fn)
-        return filenames
 
-    source_paths = search_recur(paths.homelinks())
-    relative_paths = [
-        path[len(str(paths.homelinks())) :].lstrip("/") for path in source_paths
-    ]
-    target_paths = [(paths.dest / path) for path in relative_paths]
+            source_paths.append(child)
+            relpath = child.relative_to(paths.homelinks())
+            relative_paths.append(relpath)
+
+            # apply path overrides
+            if str(relpath) in overrides:
+                target_paths.append(paths.dest / overrides[str(relpath)])
+            else:
+                target_paths.append(paths.dest / relpath)
+
+    search_recur(paths.homelinks())
     return [DotfileEntry(*t) for t in zip(source_paths, target_paths, relative_paths)]
 
 
-def get_parent_dirs(root, target_path):
-    dirs = []
-    while target_path != root and len(target_path) > 0:
-        target_path = os.path.dirname(target_path)
-        dirs.append(target_path)
-    dirs.reverse()
-    return dirs
-
-
-def get_available_customs():
-    return [os.path.basename(f) for f in glob.glob("all_customs/*")]
+def get_available_customs(paths):
+    return [p.name for p in paths.top.glob("all_customs/*")]
 
 
 ################################
@@ -89,40 +106,40 @@ class LinkOperations:
     def __init__(self, paths):
         self.paths = paths
 
-    def plan_links(self):
+    def plan_links(self) -> List[Operation]:
         entries = get_dotfile_entries(self.paths)
         plan = []
         for entry in entries:
+            abs_source_path = entry.source_path.absolute()
             if not os.path.exists(entry.target_path):
-                plan.append(("create", entry))
-            elif (
-                not os.path.islink(entry.target_path)
-                or os.readlink(entry.target_path) != entry.source_path
-            ):
-                plan.append(("replace", entry))
+                plan.append(Operation("create", abs_source_path, entry.target_path))
+            elif entry.target_path.is_symlink():
+                if entry.target_path.readlink() != abs_source_path:
+                    plan.append(Operation("replace", abs_source_path, entry.target_path))
+                else:
+                    plan.append(Operation("noop", None, entry.target_path))
+            else:
+                # exists and is not a symlink
+                plan.append(Operation("replace", abs_source_path, entry.target_path))
+
         return plan
 
-    def execute_link(self, entry):
-        if os.path.exists(entry.target_path):
-            os.remove(entry.target_path)
+    def execute_link(self, operation: Operation):
+        if operation.action == "noop":
+            return
+
+        if os.path.exists(operation.dest_path):
+            print("Removing {}".format(operation.dest_path))
+            os.remove(operation.dest_path)
 
         # make parent directories if necessary
-        parent_dirs = get_parent_dirs(os.environ["HOME"], entry.target_path)
-        for pd in parent_dirs:
-            if not os.path.isdir(pd):
-                os.mkdir(pd)
+        target_dir = operation.dest_path.parent
+        print("Creating directory {}".format(target_dir))
+        os.makedirs(target_dir, exist_ok=True)
 
-        os.symlink(entry.source_path, entry.target_path)
-
-
-class CustomsOperations:
-    def plan_customs(self):
-        return get_available_customs()
-
-    def execute_customs(self, selection):
-        if os.path.exists("customs"):
-            os.remove("customs")
-        os.symlink(selection, "customs")
+        # get the absolute path of source_path
+        print("Linking {} -> {}".format(operation.source_path, operation.dest_path))
+        os.symlink(operation.source_path, operation.dest_path)
 
 
 ################################
@@ -155,26 +172,6 @@ def print_help():
     print("sudo apt install ansible")
 
 
-def pick_customs_cli():
-    customs_ops = CustomsOperations()
-    customs_sets = customs_ops.plan_customs()
-    print(customs_sets)
-
-    # pick machine
-    selection = "NONE"
-    while selection not in customs_sets:
-        print()
-        print("Select a machine:")
-        for cs in customs_sets:
-            print("  {} ({} files)".format(cs, len(os.listdir("all_customs/" + cs))))
-        selection = input("> ")
-
-    # show dotfiles, confirm
-    print("Linking {} -> customs".format(selection))
-    customs_ops.execute_customs(selection)
-    print("Done")
-
-
 def make_links_cli():
     paths = Paths()
     link_ops = LinkOperations(paths)
@@ -182,50 +179,53 @@ def make_links_cli():
 
     # pick files
     toapply = []
-    for action, entry in planned_links:
-        print("Filename of symlink: {}".format(entry.target_path))
+    for operation in planned_links:
+        print("Filename of symlink: {}".format(operation.dest_path))
 
-        if action == "create":
-            confirm = get_confirm("Link {} (Y/n)? ".format(entry.relative_path), "y")
-        elif action == "replace":
-            print_current_status(entry.target_path)
+        relative_path = operation.source_path.relative_to(paths.homelinks())
+
+        if operation.action == "create":
+            confirm = get_confirm("Link {} (Y/n)? ".format(relative_path), "y")
+        elif operation.action == "replace":
+            print_current_status(operation.dest_path)
             confirm = get_confirm(
-                "Remove and link {} (Y/n)? ".format(entry.relative_path), "y"
+                "Remove and link {} (Y/n)? ".format(relative_path), "y"
             )
 
         if confirm == "y":
-            toapply.append(entry)
+            toapply.append(operation)
 
     # make links
-    for entry in toapply:
-        print("Removing {}".format(entry.target_path))
-        print("Creating directory {}".format(os.path.dirname(entry.target_path)))
-        print("Linking {} -> {}".format(entry.source_path, entry.target_path))
-        link_ops.execute_link(entry)
+    for operation in toapply:
+        link_ops.execute_link(operation)
 
     print("Done")
     print_help()
 
 
-def main(argv=sys.argv):
-    selection = ""
-    while selection != "x":
-        print()
-        print("apply.py:")
-        print("  c: pick customs")
-        print("  l: make symlinks to home directory")
-        print("  h: help")
+def main():
+    parser = argparse.ArgumentParser(description="Dotfile Symlink Manager")
+    parser.add_argument("command", choices=["interactive", "printops"], default="interactive", nargs="?")
+    args = parser.parse_args()
 
-        selection = input("? ")
-        if selection == "c":
-            pick_customs_cli()
-        elif selection == "l":
-            make_links_cli()
-        elif selection == "h":
-            print_help()
+    if args.command == "interactive":
+        selection = ""
+        while selection != "x":
+            print()
+            print("apply.py:")
+            print("  l: make symlinks to home directory")
+            print("  h: help")
 
-    return 0
+            selection = input("? ")
+            if selection == "l":
+                make_links_cli()
+            elif selection == "h":
+                print_help()
+
+    elif args.command == "printops":
+        ops = LinkOperations(Paths()).plan_links()
+        pprint.pp(ops)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
--- test_apply.py
+++ test_apply.py
@@ -1,11 +1,96 @@
+import pprint
 import tempfile
 from pathlib import Path
 
 from apply import *
 
 
+def test_path_overrides():
+    with tempfile.TemporaryDirectory() as td:
+        paths = Paths(top=Path("testdata/basic"), dest=td)
+        po = paths.path_overrides()
+
+        # Overridden in customs
+        assert po["vscode/settings.json"] == "Library/Application Support/Code/User/settings.json"
+
+
+
 def test_get_dotfile_entries():
     with tempfile.TemporaryDirectory() as td:
         paths = Paths(top=Path("testdata/basic"), dest=td)
-        print(get_dotfile_entries(paths))
-    assert False
+        entries = set(get_dotfile_entries(paths))
+        pprint.pp(entries)
+
+        # Ordinary homelink
+        assert DotfileEntry(
+            source_path=paths.homelinks() / ".gitconfig",
+            target_path=paths.dest / ".gitconfig",
+            relative_path=Path(".gitconfig"),
+        ) in entries
+
+        # _link_individual
+        assert DotfileEntry(
+            source_path=paths.homelinks() / ".ssh" / "config",
+            target_path=paths.dest / ".ssh" / "config",
+            relative_path=Path(".ssh/config"),
+        ) in entries
+
+        # Overridden in custom paths file
+        assert DotfileEntry(
+            source_path=paths.homelinks() / "vscode/settings.json",
+            target_path=paths.dest / "Library/Application Support/Code/User/settings.json",
+            relative_path=Path("vscode/settings.json"),
+        ) in entries
+
+
+def test_get_available_customs():
+    with tempfile.TemporaryDirectory() as td:
+        paths = Paths(top=Path("testdata/basic"), dest=td)
+        customs = set(get_available_customs(paths))
+        assert customs == {"custom1", "custom2"}
+
+
+def test_plan_execute():
+    with tempfile.TemporaryDirectory() as td:
+        paths = Paths(top=Path("testdata/basic"), dest=td)
+        link_ops = LinkOperations(paths)
+
+        # Prepare dest
+        (paths.dest / ".bashrc").touch()
+        os.symlink(src=paths.homelinks().absolute() / ".zshrc", dst=paths.dest / ".zshrc")
+
+        planned_links = set(link_ops.plan_links())
+        pprint.pp(planned_links)
+
+        # Ordinary homelink
+        assert Operation(
+            action="create",
+            source_path=paths.homelinks().absolute() / ".gitconfig",
+            dest_path=paths.dest / ".gitconfig",
+        ) in planned_links
+
+        # Ordinary homelink, but target already exists
+        assert Operation(
+            action="replace",
+            source_path=paths.homelinks().absolute() / ".bashrc",
+            dest_path=paths.dest / ".bashrc",
+        ) in planned_links
+
+        # Link already exists
+        assert Operation(
+            action="noop",
+            source_path=None,
+            dest_path=paths.dest / ".zshrc",
+        ) in planned_links
+
+        # Execute planned links
+        for operation in planned_links:
+            link_ops.execute_link(operation)
+
+        # Check result
+        assert (paths.dest / ".gitconfig").is_symlink()
+        assert (paths.dest / ".bashrc").is_symlink()
+        assert (paths.dest / ".zshrc").is_symlink()
+
+        assert (paths.dest / ".ssh/config").is_symlink() # _link_individual
+        assert (paths.dest / "Library/Application Support/Code/User/settings.json").is_symlink() # custom path_overrides
